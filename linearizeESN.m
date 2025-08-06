f8038d58e228d0a5a338af0e0c7574e88a92a816
marcel-thi <marcelthielke@web.de>
2025-08-06 16:48:21 +0200

    Add files via upload

---
+++ linearizeESN.m
@@ -0,0 +1,29 @@
+function [A_lin, B_lin] = linearizeESN(Wres, Win, xm, um, alpha)
+    % Anzahl der Neuronen
+    N = size(Wres, 1);
+    K = length(um);  % Anzahl der Eingänge
+
+    % Initialisieren
+    A_lin = zeros(N, N);
+    B_lin = zeros(N, K);  % Jetzt Matrix, nicht Vektor
+
+    % Berechne z_m = Win * um + Wres * xm
+    z_m = Win * um + Wres * xm;  % Größe N x 1
+
+    % Berechne sech^2(z) = 1 - tanh(z)^2
+    sech2 = 1 - tanh(z_m).^2;  % Elementweise
+
+    % Baue A_lin (N x N)
+    for i = 1:N
+        A_lin(i, :) = Wres(i, :) * sech2(i);
+    end
+    A_lin = (1 - alpha) * eye(N) + alpha * A_lin;
+
+    % Baue B_lin (N x K)
+    for i = 1:N
+        for k = 1:K
+            B_lin(i, k) = Win(i, k) * sech2(i);
+        end
+    end
+    B_lin = alpha * B_lin;
+end
